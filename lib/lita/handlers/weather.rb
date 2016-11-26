0e61fa26f26a78b1f27f7b226c454169db8c4ee0
Loic Shyaka <loicshyaka09@gmail.com>
2016-11-26 20:17:03 +0800

    Initial

--- lib/lita/handlers/weather.rb
+++ lib/lita/handlers/weather.rb
@@ -1,70 +1,84 @@
 
 require 'open-uri'
 require 'json'
+require 'ostruct'
+
+class String
+  def underscore
+    self.gsub(/::/, '/').
+    gsub(/ /, '_').
+    gsub(/'/, '_').
+    gsub(/([A-Z]+)([A-Z][a-z])/,'\1_\2').
+    gsub(/([a-z\d])([A-Z])/,'\1_\2').
+    gsub(/\(/, '').
+    gsub(/\)/, '').
+    tr("-", "_").
+    downcase
+  end
+end
 
 module Lita
   module Handlers
     class Weather < Handler
 
-      def populate_week
-        dates = Hash.new
-        today = Date.today
-        dates["today"] = today.strftime("%Y/%m/%d")
-        dates["tonight"] = today.strftime("%Y/%m/%d")
-        dates["tomorow"] = (today + 1).strftime("%Y/%m/%d")
-        dates["yesterday"] = (today - 1).strftime("%Y/%m/%d")
+      config :api_key, type: String,
+                       required: true
+      config :default_city, type: String,
+                       required: true
 
-        direction = "->"
-        current_day = today
-
-        7.times do |i|
-          if direction == "->"
-            direction = current_day.strftime("%A") == "Sunday" ? "<-" : "->"
-            next_day = (current_day + 1)
-            dates[next_day.strftime("%A").downcase] = next_day.strftime("%Y/%m/%d") unless current_day.strftime("%A") == "Sunday"
-            current_day = current_day.strftime("%A") == "Sunday" ? today : next_day
-          else
-            direction = current_day.strftime("%A") == "Monday" ? "->" : "<-"
-            next_day = (current_day - 1)
-            dates[next_day.strftime("%A").downcase] = next_day.strftime("%Y/%m/%d")
-            current_day = next_day
-          end
-
-        end
-        return dates
-      end
+      DAYS = %w(
+        MONDAY
+        TUESDAY
+        WEDNESDAY
+        THURSDAY
+        FRIDAY
+        SATURDAY
+        SUNDAY
+      )
 
       route(
-        /wheather\s((?:\S+\s+){1}\S+).*/i,
-        :wheather,
+        /weather\s((?:\S+\s+){0,}\S+).*/i,
+        :weather,
         :command => true,
         :help => {
           "weather forecast for specific period" => "Will provide you wheather forecst for desired "
         }
       )
 
-      def wheather(response)
-        obj = response.matches.first[0].split(" ")
-        stat = obj[0]
-        date = obj[1]
-        week = populate_week
-
-        body = get_forecast_for_date(date)
-        response.reply(body)
-
+      def find_city(name)
+        file = File.read('cities.json')
+        cities = JSON.parse(file)
+        cities[name].nil? ? nil : OpenStruct.new(cities[name])
       end
 
-      def get_forecast_for_date(date)
-        open('http://api.wunderground.com/api/daf4bfd37ff44947/forecast/q/CA/San_Francisco.json') do |f|
+      def weather(response)
+        obj = response.matches.first[0].split(" ")
+        days = obj.select{|day| DAYS.include?(day.upcase)}
+        city = obj.index("in").nil? ? find_city(Lita.config.handlers.weather.default_city.downcase.underscore) : find_city(obj.drop(obj.index("in") + 1).join("_").downcase.underscore)
+        unless city.nil?
+          week = get_forecast_for_week(city)
+          day = days.empty? ? Time.now.strftime("%A").downcase : days.first.downcase
+          forecast = week[day].nil? ? "Sorry I was unable to fetch the forecast." : week[day]
+          response.reply(forecast)
+        else
+          response.reply("Please provide a valid city name.")
+        end
+      end
+
+      # gets 10 day forecast  
+      def get_forecast_for_week(city)
+        week = Hash.new
+        open("http://api.wunderground.com/api/#{Lita.config.handlers.weather.api_key}/forecast10day/q/#{city.region}/#{city.name}.json") do |f|
           json_string = f.read
           parsed_json = JSON.parse(json_string)
-          # location = parsed_json['location']['city']
-          # temp_f = parsed_json['current_observation']['temp_f']
-          return "#{json_string}\n"
+          periods = parsed_json['forecast']['txt_forecast']['forecastday']
+          periods.each {|period| week[period["title"].downcase] = period["fcttext_metric"]}
         end
+        return week
       end
 
       Lita.register_handler(self)
     end
   end
 end
+
